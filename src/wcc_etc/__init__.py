7eb47c8075629bc9fea99c65e0701bdebe812fdc
MickaelRigault <mick.rigault@gmail.com>
2026-03-09 15:16:50 +0100

    remove wcc_etc as entry point of the API (warning)

--- src/wcc_etc/__init__.py
+++ src/wcc_etc/__init__.py
@@ -4,7 +4,8 @@ __version__ = importlib.metadata.version(__package__ or "wcc_etc")
 
 __all__ = [ "__version__"]
 
-from .wcc_etc import WCCETC
+# from .wcc_etc import WCCETC
+
 from .io import read_config, get_sensor_config
 from .simulation import Simulation
 from .sensor import Sensor
--- src/wcc_etc/wcc_etc.py
+++ src/wcc_etc/wcc_etc.py
@@ -17,6 +17,11 @@ from . import psfsim
 from .io import read_config, PACKAGE_PATH
 
 
+
+import warnings
+warnings.warn("DEPRECATION: wcc_etc.py is deprecated.")
+
+
 #   One line function prepends the support path to variable s2 if s1 is provided, else returns s2 as is.
 prepend_if_not_none = lambda s1, s2: f"{s1}{s2}" if s1 is not None else s2
 
